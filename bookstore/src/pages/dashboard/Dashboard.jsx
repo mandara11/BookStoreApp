0f094cbdb47bda1c1eb16203897d64bab034576d
mandara11 <mandaram2345@gmail.com>
2022-11-17 20:15:42 +0530

    Added:pagination

--- bookstore/src/pages/dashboard/Dashboard.jsx
+++ bookstore/src/pages/dashboard/Dashboard.jsx
@@ -9,6 +9,9 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { getBookList } from '../../services/dataService';
 import BookSummary from '../../components/booksummary/BookSummary';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import HomeIcon from '@mui/icons-material/Home';
 
 
 
@@ -64,7 +67,7 @@ const useStyles = makeStyles({
         alignItems: 'center',
         fontSize: '11px',
         position: 'relative',
-        top: '30px',
+        top: '40px',
         backgroundColor: '#2E1D1E',
         color: '#FFFFFF',
     },
@@ -82,8 +85,14 @@ function Dashboard(props) {
     const [toggle1, setToggle1] = useState(false)
     const [input, setInput] = useState({})
 
+    const [search, setSearch] = useState("")
+
+    const [page, setPage] = useState(1)
+    const [pagination, setPagination] = useState(false)
+
     const openBookSummary = (detailsObj) => {
         setToggle1(true);
+        setPagination(true)
         console.log(detailsObj)
         setInput(detailsObj)
         console.log(input.bookName, "particular book details")
@@ -102,12 +111,14 @@ function Dashboard(props) {
         })
     }, [])
 
-    
+    const autoRefresh = () => {
+        getBookList()
+    }
 
     return (
         <div>
             <Box>
-                <Header />
+                <Header onChange={event => { setSearch(event.target.value) }} />
             </Box>
             <Box className={classes.container2}>
                 <Box className={classes.header2}>
@@ -120,11 +131,54 @@ function Dashboard(props) {
                     </Box>
                 </Box>
                 <Box sx={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap' }}>
-                    {toggle1 ? <BookSummary openBookBack={openBookBack} id={input._id} bookName={input.bookName} author={input.author} quantity={input.quantity} discountPrice={input.discountPrice} price={input.price} description={input.description} />
-                     : bookList.map((book) => (<Box onClick={()=>openBookSummary(book)} ><Book key={book._id} book={book} /> </Box>))
+                    {toggle1 ? <BookSummary openBookBack={openBookBack} id={input._id} bookName={input.bookName} author={input.author} quantity={input.quantity} discountPrice={input.discountPrice} price={input.price} description={input.description}
+                        index={bookList.indexOf(input)} />
+                        :
+                        //   bookList.map((book) => (<Box onClick={()=>openBookSummary(book)} ><Book key={book._id} book={book} />
+                        //   </Box>))
+                        page === 1 ?
+                            bookList.filter((book) => {
+                                if (search == "") {
+                                    return book
+                                }
+                                else if (book.bookName.toLowerCase().includes(search.toLowerCase())) {
+                                    return book
+                                }
+                            }).slice(0, 8).map(
+                                (book) => (<Box onClick={() => openBookSummary(book)}><Book key={book._id} book={book} autoRefresh={autoRefresh}
+                                /></Box>)
+                            ) :
+                            page === 2 ?
+                                bookList.slice(8, 16).map(
+                                    (book) => (<Box onClick={() => openBookSummary(book)}><Book key={book._id} book={book} autoRefresh={autoRefresh}
+                                    /></Box>)
+                                ) :
+                                page === 3 ?
+                                    bookList.slice(16, 24).map(
+                                        (book) => (<Box onClick={() => openBookSummary(book)}><Book key={book._id} book={book} autoRefresh={autoRefresh}
+                                        /></Box>)
+                                    ) :
+                                    page === 4 ?
+                                        bookList.slice(24, 30).map(
+                                            (book) => (<Box onClick={() => openBookSummary(book)}><Book key={book._id} book={book} autoRefresh={autoRefresh}
+                                            /></Box>)
+                                        ) :
+                                        null
                     }
                 </Box>
             </Box>
+            {
+                pagination ? null :
+                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', color: '#878787', position: 'relative', top: '20px' }}>
+                    <HomeIcon color='action' />
+                    <Stack spacing={4}>
+                        <Pagination size='medium' component="div" count={4} siblingCount={0}
+
+                            onChange={(e, value) => setPage(value)}
+                        />
+                    </Stack>
+                </Box>
+            }
             <Box className={classes.footer}>
                 <Box className={classes.footertxt}>Copyright @ 2020, Bookstore Private Limited.All Rights Reserved</Box>
             </Box>
